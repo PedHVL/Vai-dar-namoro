4117878ebb356529f4955ae53f2cddd36bbcac3f
Pedro <pedrohenriquevidalleite@gmail.com>
2022-10-04 22:45:13 -0300

    projeto finalizado

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  nome1: string;
+  nome2: string;
+  resposta: string;
+
+
+  constructor(private http: HttpClient) { }
+
+  calcular() {
+
+    const urlBase = 'http://lucasreno.kinghost.net/love-calculator/';
+    const url = urlBase + this.nome1 + '/' + this.nome2;
+
+    if (!this.nome1 || !this.nome2) {
+      this.resposta = "Complete as lacunas";
+    }
+
+    else {
+      this.http.get(url).subscribe(resultado => this.resposta = resultado+"%");
+    }
+  }
+
+}
